3c3f8ea1ace80e43872c6ed62a8a4effc3a0cc01
fefe <fedescalada17@hotmail.com>
2022-08-30 23:12:13 -0300

    slider y cambio de titulo

--- header/headerVer2.js
+++ header/headerVer2.js
@@ -2,33 +2,33 @@ function headerContent(e){
     const headerE = document.createElement("div");
     headerE.innerHTML = `
     <header class="header">
-    <h4 class="header_titulo">CERRAJERIA</h4>
+    <h1 class="header_titulo">Cerrajeria Camparo</h1>
     <div class="header_burger" >
         <img class="header_burger_img" src="img/llaves-burger-img.png" onclick="sound.play()" alt="menu">
     </div>
 </header>
 
     <div class="header__menu">
-    <a class="contenido__cerraduras" href="./cerradurasVer2.html">Cerraduras</a>
-    <a class="contenido__candados" href="./candadosVer2.html">Candados</a>
-    <a class="contenido__cerrojos" href="./cerrojosVer2.html">Cerrojos</a>
-    <a class="contenido__herrajes" href="./herrajesVer2.html">Herrajes</a>
-    <a class="contenido__llaveros_c" href="./llaveroscVer2.html">Llaveros cortos</a>
-    <a class="contenido__llaveros_l" href="./llaveroslVer2.html">Llaveros largos</a>
-    <a class="contenido__pasadores" href="./pasadoresVer2.html">Pasadores</a>
+    <a class="contenido__cerraduras" href="./cerradurasVer2.html?ver2-1">Cerraduras</a>
+    <a class="contenido__candados" href="./candadosVer2.html?ver2-1">Candados</a>
+    <a class="contenido__cerrojos" href="./cerrojosVer2.html?ver2-1">Cerrojos</a>
+    <a class="contenido__herrajes" href="./herrajesVer2.html?ver2-1">Herrajes</a>
+    <a class="contenido__llaveros_c" href="./llaveroscVer2.html?ver2-1">Llaveros cortos</a>
+    <a class="contenido__llaveros_l" href="./llaveroslVer2.html?ver2-1">Llaveros largos</a>
+    <a class="contenido__pasadores" href="./pasadoresVer2.html?ver2-1">Pasadores</a>
     </div>
     <section class="header__ventana">
     <div class="header__ventana-cierra">
     <button class="header__ventana-cierra-b">X</button>
     </div>
     <div class="header__ventana__contenido">
-    <a class="contenido__cerraduras" href="./cerradurasVer2.html">Cerraduras</a>
-    <a class="contenido__candados" href="./candadosVer2.html">Candados</a>
-    <a class="contenido__cerrojos" href="./cerrojosVer2.html">Cerrojos</a>
-    <a class="contenido__herrajes" href="./herrajesVer2.html">Herrajes</a>
-    <a class="contenido__llaveros_c" href="./llaveroscVer2.html">Llaveros cortos</a>
-    <a class="contenido__llaveros_l" href="./llaveroslVer2.html">Llaveros largos</a>
-    <a class="contenido__pasadores" href="./pasadoresVer2.html">Pasadores</a>
+    <a class="contenido__cerraduras" href="./cerradurasVer2.html?ver2-1">Cerraduras</a>
+    <a class="contenido__candados" href="./candadosVer2.html?ver2-1">Candados</a>
+    <a class="contenido__cerrojos" href="./cerrojosVer2.html?ver2-1">Cerrojos</a>
+    <a class="contenido__herrajes" href="./herrajesVer2.html?ver2-1">Herrajes</a>
+    <a class="contenido__llaveros_c" href="./llaveroscVer2.html?ver2-1">Llaveros cortos</a>
+    <a class="contenido__llaveros_l" href="./llaveroslVer2.html?ver2-1">Llaveros largos</a>
+    <a class="contenido__pasadores" href="./pasadoresVer2.html?ver2-1">Pasadores</a>
     </div>
     </section>
     `
